0aebd2bb48360d51e5c0a241d066d164597c5f6a
Sol Irvine <sol@turnstilelive.com>
2017-02-23 23:25:15 +0900

    [WIP] Debugging redirect issue.

--- lib/letsencrypt-rails-heroku/middleware.rb
+++ lib/letsencrypt-rails-heroku/middleware.rb
@@ -6,8 +6,12 @@ module Letsencrypt
     end
 
     def call(env)
+      Rails.logger.debug "Seeking challenge at /#{Letsencrypt.configuration.acme_challenge_filename}"
       if Letsencrypt.challenge_configured? && env["PATH_INFO"] == "/#{Letsencrypt.configuration.acme_challenge_filename}"
+        Rails.logger.debug "Challenge accepted. Should return 200."
         return [200, {"Content-Type" => "text/plain"}, [Letsencrypt.configuration.acme_challenge_file_content]]
+      else
+        Rails.logger.debug "Challenge rejected. #{@app}"
       end
 
       @app.call(env)
