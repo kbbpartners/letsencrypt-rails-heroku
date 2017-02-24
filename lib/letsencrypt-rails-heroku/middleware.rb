c789990051ab5ab096c16b822800b9ba5120e09e
Sol Irvine <sol@turnstilelive.com>
2017-02-24 20:46:48 +0900

    Modified refresh on config

--- lib/letsencrypt-rails-heroku/middleware.rb
+++ lib/letsencrypt-rails-heroku/middleware.rb
@@ -9,12 +9,12 @@ module Letsencrypt
       Rails.logger.info "Letsencrypt::Middleware called..."
       Rails.logger.info "LE 01A - Current path is #{env["PATH_INFO"]}"
       Rails.logger.info "LE 01B - Expected path is #{Letsencrypt.configuration.acme_challenge_filename}"
-      Rails.logger.info "LE 01C - ENV['ACME_CHALLENGE_FILENAME'] - #{env["ACME_CHALLENGE_FILENAME"]}"
+      Rails.logger.info "LE 01C - ENV['ACME_CHALLENGE_FILENAME'] - #{ENV["ACME_CHALLENGE_FILENAME"]}"
       if Letsencrypt.challenge_configured?
-        return [200, {"Content-Type" => "text/plain"}, [env["ACME_CHALLENGE_FILE_CONTENT"]]]
+        return [200, {"Content-Type" => "text/plain"}, [ENV["ACME_CHALLENGE_FILE_CONTENT"]]]
       else
         Rails.logger.info "LE 01D - Current path and expected path match?"
-        Rails.logger.info env["PATH_INFO"] == "/#{env["ACME_CHALLENGE_FILENAME"]}"
+        Rails.logger.info((env["PATH_INFO"] == "/#{ENV["ACME_CHALLENGE_FILENAME"]}") ? 'YES' : 'NO')
       end
 
       @app.call(env)
