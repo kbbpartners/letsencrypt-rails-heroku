db60b8412886f9ae0cbd381f5bbaafb7bd549bf7
Sol Irvine <sol@turnstilelive.com>
2017-02-24 14:16:21 +0900

    Added more debugging

--- lib/letsencrypt-rails-heroku/middleware.rb
+++ lib/letsencrypt-rails-heroku/middleware.rb
@@ -9,6 +9,8 @@ module Letsencrypt
       Rails.logger.info "Letsencrypt::Middleware called..."
       Rails.logger.info "LE 01A - Current path is #{env["PATH_INFO"]}"
       Rails.logger.info "LE 01B - Expected challenge path is #{Letsencrypt.configuration.acme_challenge_filename}"
+      Rails.logger.info "LE 01C - Expected challenge content is #{Letsencrypt.configuration.acme_challenge_file_content}"
+      Rails.logger.info "LE 01D - ENV['ACME_CHALLENGE_FILENAME'] - #{ENV["ACME_CHALLENGE_FILENAME"]}"
       if Letsencrypt.challenge_configured? && env["PATH_INFO"] == "/#{Letsencrypt.configuration.acme_challenge_filename}"
         return [200, {"Content-Type" => "text/plain"}, [Letsencrypt.configuration.acme_challenge_file_content]]
       end
