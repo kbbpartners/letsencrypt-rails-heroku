f51aa6b7ea91873fe9ed8cad80024155beda2af2
Sol Irvine <sol@turnstilelive.com>
2017-02-25 02:02:18 +0900

    Convert properties to method calls

--- lib/letsencrypt-rails-heroku/letsencrypt.rb
+++ lib/letsencrypt-rails-heroku/letsencrypt.rb
@@ -14,13 +14,11 @@ module Letsencrypt
       configuration.acme_challenge_file_content
   end
 
-  def self.update_challenge(challenge_filename, challenge_file_content)
-    configuration.acme_challenge_filename = challenge_filename
-    configuration.acme_challenge_file_content = challenge_file_content
-  end
-
   class Configuration
-    attr_accessor :heroku_token, :heroku_app, :acme_email, :acme_domain, :acme_endpoint, :acme_challenge_filename, :acme_challenge_file_content
+    attr_accessor :heroku_token, :heroku_app, :acme_email, :acme_domain, :acme_endpoint
+
+    # Not settable by user; part of the gem's behaviour.
+    attr_reader :acme_challenge_filename, :acme_challenge_file_content
 
     def initialize
       @heroku_token = ENV["HEROKU_TOKEN"]
@@ -28,8 +26,14 @@ module Letsencrypt
       @acme_email = ENV["ACME_EMAIL"]
       @acme_domain = ENV["ACME_DOMAIN"]
       @acme_endpoint = ENV["ACME_ENDPOINT"] || 'https://acme-v01.api.letsencrypt.org/'
-      # @acme_challenge_filename = ENV["ACME_CHALLENGE_FILENAME"]
-      # @acme_challenge_file_content = ENV["ACME_CHALLENGE_FILE_CONTENT"]
+    end
+
+    def acme_challenge_filename
+      ENV["ACME_CHALLENGE_FILENAME"]
+    end
+
+    def acme_challenge_file_content
+      ENV["ACME_CHALLENGE_FILE_CONTENT"]
     end
 
     def valid?
